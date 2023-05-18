7bcdb4778733a96753e89c878a7a987871e228bc
AntonioBukin <bav1905@gmail.com>
2023-05-18 13:18:59 +0300

    add first part information

---
+++ my-app/src/index.js
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom";
+
+// const elem1 = <span>Hello</span>;
+
+// const elem2 = <span>world</span>;
+
+//!Props Children - це діти вашого елементу
+
+// const element = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// ); //це js вираження
+
+// console.log(element);
+
+// ReactDOM.render(element, document.querySelector("#root"));
+//___________________________________________________________//
