650e948fb68623421f2b712b3a3a8a8974e061f6
AntonioBukin <bav1905@gmail.com>
2023-05-18 16:18:44 +0300

    add new info

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom";
+import App from "./App";
+import paintings from "./paintings.json";
 
 // const elem1 = <span>Hello</span>;
 
@@ -11,9 +13,66 @@ import ReactDOM from "react-dom";
 //     {elem1}
 //     {elem2}
 //   </div>
-// ); //це js вираження
+// ); //це js вираз
 
 // console.log(element);
 
 // ReactDOM.render(element, document.querySelector("#root"));
 //___________________________________________________________//
+
+//Відрісовка сторінки (JSX вираз)
+
+// const data = {
+//   id: "id-1",
+//   url: "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+//   title: "Feathers. Art abstract",
+//   price: 500,
+//   author: {
+//     tag: "ractapopulous",
+//     url: "https://pixabay.com/users/ractapopulous-24766/",
+//   },
+//   quantity: 10,
+// };
+
+// const painting = (
+//   <div>
+//     <img src={data.url} alt={data.title} width="480" />
+//     <h2>{data.title}</h2>
+//     <p>
+//       Автор: <a href={data.author.url}>{data.author.tag}</a>
+//     </p>
+//     <p>Цена: {data.price} кредитов</p>
+//     <p>Доступность: заканчивается или есть в наличии}</p>
+//     <button type="button">Добавить в корзину</button>
+//   </div>
+// );
+
+// ReactDOM.render(painting, document.querySelector("#root"));
+
+//___________________________________________________________//
+
+//Компоненти - це незалежні шматочки інтерфейсу
+
+//Щоб не писати повсюду Props (можна зробити деструктурізацію)
+//Ф-цію перенесли в файл Painting.js
+
+// function Painting(props) {
+//   const { url, title, profileUrl, authorName, price } = props;
+//   //console.log(Props);
+//   return (
+//     <div>
+//       <img src={url} alt={title} width="480" />
+//       <h2>{title}</h2>
+//       <p>
+//         Автор: <a href={profileUrl}>{authorName}</a>
+//       </p>
+//       <p>Цена: {price}кредитов</p>
+//       <p>Доступность: заканчивается или есть в наличии</p>
+//       <button type="button">Добавить в корзину</button>
+//     </div>
+//   );
+// }
+
+// const painting = paintings[2];
+
+ReactDOM.render(<App />, document.querySelector("#root"));
